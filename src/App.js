bf54875a394c972bb3665cb84210d1c60b8e8999
nabsabraham <nabila.abraham@gmail.com>
2024-01-23 20:44:59 -0500

    commit dump

--- src/App.js
+++ src/App.js
@@ -1,25 +1,35 @@
-import logo from './logo.svg';
-import './App.css';
+/*App.js*/
+
+import React from 'react';
+import { GoogleLogin } from '@react-oauth/google';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    const responseMessage = (response) => {
+        console.log(response);
+        if (response.credential){ 
+          fetch('http://localhost:8000/auth?token='+ response.credential,{ 
+            credentials: 'include', 
+            // To cause browsers to send a request with credentials included on both same-origin and cross-origin calls,  
+            // add credentials: 'include' to the init object you pass to the fetch() method. 
+           }) 
+          .then((response) => { 
+            return response.json(); 
+          }) 
+          .then((myJson) => { 
+            alert(myJson) 
+          }); 
+        }
+    };
+    const errorMessage = (error) => {
+        console.log(error);
+    };
+    return (
+        <div>
+            <h2>React Google Login</h2>
+            <br />
+            <br />
+            <GoogleLogin onSuccess={responseMessage} onError={errorMessage} />
+        </div>
+    )
 }
-
 export default App;
